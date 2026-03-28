fc7b9aec920fc7486477571f6c2550fe9c6ba71e
siillvergun <duejql03@naver.com>
2026-03-28 18:03:29 +0900

    chore: BE 개발 초기 설정

---
+++ gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/AppcenterTodoApiApplication.java
@@ -0,0 +1,13 @@
+package com.siillvergun.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcenterTodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcenterTodoApiApplication.class, args);
+	}
+
+}
---
+++ gunsilver/appcenter-todo-api/src/test/java/com/siillvergun/todolist/AppcenterTodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siillvergun.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcenterTodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
