731ca5a733325532e3f1fc28208237c7cdd06a74
siillvergun <duejql03@naver.com>
2026-03-29 16:31:25 +0900

    feat: 요청, 응답 dto 기본 구조 개발 완료

--- gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/dto/TodoRequestDto.java
+++ gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/dto/TodoRequestDto.java
@@ -1,4 +1,16 @@
 package com.siillvergun.todolist.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TodoRequestDto {
+    private Long id;
+    private String content;
+    private String category;
 }
--- gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/dto/TodoResponseDto.java
+++ gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/dto/TodoResponseDto.java
@@ -1,4 +1,15 @@
 package com.siillvergun.todolist.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TodoResponseDto {
+    private String content;
+    private String category;
 }
