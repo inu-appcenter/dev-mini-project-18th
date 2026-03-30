d17db52fad00819adaf8adb627d306ae8ee2a669
mangcho3542 <qgeon0419@gmail.com>
2026-03-31 00:55:35 +0900

    feat: FE(박건) Props(DivProps, ButtonProps, InputProps, TableProps, TheadProps, TrProps, ThProps, TdProps)들 추가
    다른 컴포넌트들에서 extends 혹은 사용하기 위해 기본적인 HTMLElement들의 props type들 추가함.

---
+++ chong/src/types/Props.ts
@@ -0,0 +1,12 @@
+import React from "react";
+type ComponentProps<T extends React.ElementType> = React.ComponentPropsWithRef<T>;
+
+export type DivProps = ComponentProps<"div">;
+export type ButtonProps = ComponentProps<"button">;
+export type InputProps = ComponentProps<"input">;
+
+export type TableProps = ComponentProps<"table">;
+export type TheadProps = ComponentProps<"thead">;
+export type TrProps = ComponentProps<"tr">;
+export type ThProps = ComponentProps<"th">;
+export type TdProps = ComponentProps<"td">;
