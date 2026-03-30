9e728bc6ff7d8c71e333219302c37fef301bcca7
mangcho3542 <qgeon0419@gmail.com>
2026-03-30 23:36:20 +0900

    chore: FE 개발 환경 초기 설정

---
+++ chong/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    // svgr 설정 추가
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
