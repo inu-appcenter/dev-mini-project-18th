1440aefd08f4116f8789abe27ded0c8a0d5e2e4c
siillvergun <duejql03@naver.com>
2026-03-31 15:16:13 +0900

    feat: read api 개발 및 테스트 완료

--- gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/service/TodoService.java
+++ gunsilver/appcenter-todo-api/src/main/java/com/siillvergun/todolist/todo/service/TodoService.java
@@ -8,14 +8,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepository;
 
     @Transactional
-    public TodoResponseDto createTodo(TodoRequestDto todoRequestDto){
+    public TodoResponseDto createTodo(TodoRequestDto todoRequestDto) {
         Todo todo = todoRequestDto.toEntity();
         return TodoResponseDto.from(todoRepository.save(todo));
+    }
+
+    public List<TodoResponseDto> getAllTodo() {
+        List<Todo> todos = todoRepository.findAll();
+        return todos.stream().map(TodoResponseDto::from).toList();
     }
 }
