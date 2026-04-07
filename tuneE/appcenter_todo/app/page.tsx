6687648269a26a0a4af3e3d874eac51159a1de50
김태윤 <xodbs1758@gmail.com>
2026-04-08 00:12:57 +0900

    style: 주석 수정

--- tuneE/appcenter_todo/app/page.tsx
+++ tuneE/appcenter_todo/app/page.tsx
@@ -4,7 +4,7 @@ import HomeClient from '@/components/client/HomeClient';
 
 export default async function Home() {
   // 서버 컴포넌트에서 데이터를 fetch하여 마운트 이전에도 데이터 로딩 가능(SSR)
-  // 서버 컴포넌트에서 보내는 서버로 보내는 fetch 요청은 CORS 정책에서 자유롭다
+  // 서버 컴포넌트에서 서버로 보내는 fetch 요청은 CORS 정책에서 자유롭다
   // 서버 URL 수정예정
   const response = await fetch('http://localhost:8080/todos');
   const Todos = await response.json();
