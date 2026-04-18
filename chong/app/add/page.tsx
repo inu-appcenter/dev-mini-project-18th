4e52cdc4f5fc1ac3149a151c6e75de15e07fe9d0
mangcho3542 <qgeon0419@gmail.com>
2026-04-18 20:15:16 +0900

    chore: Suspense로 컴포넌트 감싸기 추가

--- chong/app/add/page.tsx
+++ chong/app/add/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import ClientAdd from "./client";
 import { Metadata } from "next";
 
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function Add() {
-    return <ClientAdd />;
+    return (
+        <Suspense>
+            <ClientAdd />
+        </Suspense>
+    );
 }
