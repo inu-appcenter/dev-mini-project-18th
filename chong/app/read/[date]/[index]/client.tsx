7ff9eacc5621a843dbf46bc25d50ae608dcd323e
mangcho3542 <qgeon0419@gmail.com>
2026-04-21 09:41:56 +0900

    refactor: ReadClient에서 dayjs import 삭제

--- chong/app/read/[date]/[index]/client.tsx
+++ chong/app/read/[date]/[index]/client.tsx
@@ -1,7 +1,6 @@
 "use client";
 import TodoList from "@/components/TodoList";
 import DateDial from "@/components/DateDial";
-import dayjs from "@constants/dayjs";
 
 interface Props {
     date: string;
@@ -9,12 +8,10 @@ interface Props {
 }
 
 export default function ReadClient({ date, index }: Props) {
-    const d = dayjs(date);
-
     return (
         <>
-            <DateDial date={d} index={index} />
-            <TodoList date={date} index={index} />
+            <DateDial date={date} index={index} />
+            <TodoList date={date} />
         </>
     );
 }
